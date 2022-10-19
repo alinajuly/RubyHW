64f560108c199296e246619d19fd5502d1514e50
alinajuly <bluealinajuly90@gmail.com>
2022-10-20 00:04:14 +0300

    add 2 homework

---
+++ HW1/2.rb
@@ -0,0 +1,15 @@
+p 'Дан целочисленный массив. Необходимо вывести вначале его элементы с нечетными индексами, а затем - четными.'
+
+p array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+p 'odd_even_array = array.partition.each_with_index { |_, i| i.odd? }.flatten'
+
+    odd_even_array = array.partition.each_with_index { |_, i| i.odd? }.flatten
+
+p odd_even_array
+
+p 'odd_even_array = array.select.each_with_index {|_, i| i.odd?} + array.select.each_with_index{|_, i| i.even?}'
+
+    odd_even_array = array.select.each_with_index {|_, i| i.odd?} + array.select.each_with_index{|_, i| i.even?}
+	
+p odd_even_array
