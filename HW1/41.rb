20bf4bb43c6b46f1bfd869dc935006100165fb61
alinajuly <bluealinajuly90@gmail.com>
2022-10-20 20:48:50 +0300

    add 41 homework

---
+++ HW1/41.rb
@@ -0,0 +1,9 @@
+p 'Дан целочисленный массив. Найти минимальный четный элемент.'
+
+p array = [9, -2, 3, 12, -10, 1, 3, -3, 10]
+
+p 'new_array = array.select { |number| number.even? }.min'
+
+new_array = array.select { |number| number.even? }.min
+
+p new_array
