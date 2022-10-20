e280fb454c7e4dc4a09fa16da67903ef08b91164
alinajuly <bluealinajuly90@gmail.com>
2022-10-20 19:43:04 +0300

    add 29 homework

---
+++ HW1/29.rb
@@ -0,0 +1,9 @@
+p 'Дан целочисленный массив. Упорядочить его по возрастанию.'
+
+p array = [9, -2, 3, 2, -10, -1, 3, -3, 10, 1]
+
+p 'new_array = array.sort'
+
+new_array = array.sort
+
+p new_array
