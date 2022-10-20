ee14de5113a657362b20b4efe86e17c947e12f28
alinajuly <bluealinajuly90@gmail.com>
2022-10-20 16:20:35 +0300

    add 7 homework

---
+++ HW1/7.rb
@@ -0,0 +1,9 @@
+'Дан целочисленный массив. Преобразовать его, прибавив к нечетным числам последний элемент. Первый и последний элементы массива не изменять.'
+
+p array = [9, -95, 70, 44, 15, 37, 10, -20, 17, 30]
+
+p 'new_array = array.map.with_index { |element, index| ![0, array.length - 1].include?(index) && element.odd? && element + array[array.length - 1] || element }'
+
+new_array = array.map.with_index { |element, index| ![0, array.length - 1].include?(index) && element.odd? && element + array[array.length - 1] || element }
+
+p new_array
