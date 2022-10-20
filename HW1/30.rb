dbfe62c2b101c424a4ac12eef5a6ff6681d286ea
alinajuly <bluealinajuly90@gmail.com>
2022-10-20 19:43:54 +0300

    add 30 homework

---
+++ HW1/30.rb
@@ -0,0 +1,9 @@
+p 'Дан целочисленный массив. Упорядочить его по убыванию.'
+
+p array = [9, -2, 3, 2, -10, -1, 3, -3, 10, 1]
+
+p 'new_array = array.sort.reverse!'
+
+new_array = array.sort.reverse!
+
+p new_array
