610b6671e9fb03b08b22761c412dafe4cf53a326
alinajuly <bluealinajuly90@gmail.com>
2022-10-20 18:23:32 +0300

    add 10 homework

---
+++ HW1/10.rb
@@ -0,0 +1,9 @@
+p 'Дан целочисленный массив. Заменить все положительные элементы на значение максимального.'
+
+p array = [9, -95, 70, 44, -15, 37, 10, -20, 17, -30]
+
+p 'new_array = array.map { |element| element > 0 ? array.select{ |value| value > 0 }.max : element} '
+
+new_array = array.map { |element| element > 0 ? array.select{ |value| value > 0 }.max : element} 
+
+p new_array
