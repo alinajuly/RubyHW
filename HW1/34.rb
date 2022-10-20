39afcc78c21de4c6dd864f6740150019770f52bf
alinajuly <bluealinajuly90@gmail.com>
2022-10-20 19:45:01 +0300

    add 34 homework

---
+++ HW1/34.rb
@@ -0,0 +1,9 @@
+p 'Дан целочисленный массив. Найти индекс максимального элемента.'
+
+p array = [9, -2, 3, 2, -10, -1, 3, -3, 10, 1]
+
+p 'new_array = array.index(array.max)'
+
+new_array = array.index(array.max)
+
+p new_array
