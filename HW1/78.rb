ff27717e484e70c0f67f6e529712a983ada53d81
alinajuly <bluealinajuly90@gmail.com>
2022-10-20 21:38:29 +0300

    add 78 homework

---
+++ HW1/78.rb
@@ -0,0 +1,9 @@
+p 'Дано целое число. Найти произведение его цифр.'
+
+p array = [9, -2, 3, 12, -10, 1, 3, -3, 10]
+
+p 'multiply_array = array.inject(:*)'
+
+multiply_array = array.inject(:*)
+
+p multiply_array
