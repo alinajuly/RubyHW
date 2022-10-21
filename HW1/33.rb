fef740264aeadcb8b88ac0af7764752ef8413162
alinajuly <bluealinajuly90@gmail.com>
2022-10-21 23:03:36 +0300

    edit sintaxis

--- HW1/33.rb
+++ HW1/33.rb
@@ -1,9 +1,7 @@
 p 'Дан целочисленный массив. Найти индекс минимального элемента.'
 
-p array = [9, -2, 3, 2, -10, -1, 3, -3, 10, 1]
+p array = [9, -2, 3, -3, 10, 1]
 
-p 'new_array = array.index(array.min)'
+p array.index(array.min)
 
-new_array = array.index(array.min)
-
-p new_array
+p array.index(array.min)
