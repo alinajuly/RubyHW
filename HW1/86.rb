893948f90737c6d62e7bcbb502226e0140fcc8fc
alinajuly <bluealinajuly90@gmail.com>
2022-10-21 15:27:55 +0300

    add 86 homework

---
+++ HW1/86.rb
@@ -0,0 +1,9 @@
+p 'Дан целочисленный массив. Найти среднее арифметическое его элементов.'
+
+p array = [9, -2, 3, 12, -10, 1, 3, -3, 10]
+
+p 'arithmetic_mean_array = array.sum / array.size'
+
+arithmetic_mean_array = array.sum / array.size
+
+p arithmetic_mean_array
