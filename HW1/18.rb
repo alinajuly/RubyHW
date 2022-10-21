29f039e7e04076e900fbfeb83a05dbe65f89f2cc
alinajuly <bluealinajuly90@gmail.com>
2022-10-21 20:35:55 +0300

    add variable

--- HW1/18.rb
+++ HW1/18.rb
@@ -2,8 +2,8 @@ p 'Дан целочисленный массив. Найти количеств
 
 p array = [10, 3, 9, 0, -4, 22, 9]
 
-p 'new_array = array.each_index.select { |i| array[i] < array[i - 1] && array[i + 1] && array[i] < array[i + 1] }.count'
+p 'count_local_min = array.each_index.select { |i| array[i] < array[i - 1] && array[i + 1] && array[i] < array[i + 1] }.count'
 
-new_array = array.each_index.select { |i| array[i] < array[i - 1] && array[i + 1] && array[i] < array[i + 1] }.count
+count_local_min = array.each_index.select { |i| array[i] < array[i - 1] && array[i + 1] && array[i] < array[i + 1] }.count
 
-p new_array
+p count_local_min
