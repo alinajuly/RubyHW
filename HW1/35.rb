a62279a75950b147483cccec01f69ddcb91ba60d
alinajuly <bluealinajuly90@gmail.com>
2022-10-21 23:09:47 +0300

    add another solution

--- HW1/35.rb
+++ HW1/35.rb
@@ -1,13 +1,7 @@
 p 'Дан целочисленный массив. Найти индекс первого минимального элемента.'
 
-p array = [10, 3, 9, 0, -4, 22, 9]
+p array = [10, -4, 9, 0, -4, 22, 9]
 
-# p 'index_min = array.each_index.find { |i| array[i] < array[i - 1] }'
+p 'array.index(array.min)'
 
-# # index_min = array.each_index.find { |i| array[i] < array[i - 1] }
-
-# p index_min
-
-index_min = array.each_index.find { |i| array[i] < array[i - 1] && array[i] < array[i + 1] }
-
-p index_min
+p array.index(array.min)
