088de99867c455eddb1aac8ae75ca912e667d058
alinajuly <bluealinajuly90@gmail.com>
2022-10-21 16:51:55 +0300

    add 79 homework

---
+++ HW1/79.rb
@@ -0,0 +1,9 @@
+p 'Дан целочисленный массив. Возвести в квадрат отрицательные элементы и в третью степень - положительные. Нулевые элементы - не изменять.	'
+
+p array = [0, -2, 3, 12, -10, 1, 3, 9, 10]
+
+p 'new_array = array.map { |element| element < 0 ? element ** 2 : element ** 3} '
+
+new_array = array.map { |element| element < 0 ? element ** 2 : element ** 3} 
+
+p new_array
