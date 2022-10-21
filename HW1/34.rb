14a611136f6c821d0464d50e8dd48d53f1ef7bfd
alinajuly <bluealinajuly90@gmail.com>
2022-10-21 23:07:12 +0300

    adit sintaxis

--- HW1/34.rb
+++ HW1/34.rb
@@ -1,9 +1,7 @@
 p 'Дан целочисленный массив. Найти индекс максимального элемента.'
 
-p array = [9, -2, 3, 2, -10, -1, 3, -3, 10, 1]
+p array = [9, -2, 3, -3, 10, 1]
 
-p 'new_array = array.index(array.max)'
+p 'array.index(array.max)'
 
-new_array = array.index(array.max)
-
-p new_array
+p array.index(array.max)
