1ee7541ab8b0eb00beac25b39e26cc0f4e4691bc
alinajuly <bluealinajuly90@gmail.com>
2022-10-25 22:52:31 +0300

    correct sintaxis

--- HW1/32.rb
+++ HW1/32.rb
@@ -2,6 +2,6 @@ p 'Дан целочисленный массив. Вывести индексы
 
 p array = [1, 3, 2, 4]
 
-p 'array.map{ |element| array.sort.index(element)}'
+p 'array.map{ |element| array.sort.index(element) }'
 
-p array.map{ |element| array.sort.index(element)}
+p array.map{ |element| array.sort.index(element) }
