394a7c13d6547294b6042fba38ff5a2f8a096794
alinajuly <bluealinajuly90@gmail.com>
2022-10-25 22:53:44 +0300

    correct sintaxis

--- HW1/41.rb
+++ HW1/41.rb
@@ -4,6 +4,8 @@ p array = [9, -2, 3, 12, -10, 1, 3, -3, 10]
 
 p 'new_array = array.select { |number| number.even? }.min'
 
-new_array = array.select { |number| number.even? }.min
+# new_array = array.select { |number| number.even? }.min
+
+new_array = array.select { &:even? }.min
 
 p new_array
