5322b57bfededf1b327f6fc6dbb0eecb4ad9c0cf
alinajuly <bluealinajuly90@gmail.com>
2022-10-25 22:48:53 +0300

    correct sintaxis

--- HW1/9.rb
+++ HW1/9.rb
@@ -2,8 +2,8 @@ p 'Дан целочисленный массив. Заменить все по�
 
 p array = [9, -95, 70, 44, 15, 37, 10, -20, 17, -30]
 
-p 'new_array = array.map { |element| element > 0 ? array.select{ |value| value > 0 }.min : element} '
+p 'new_array = array.map { |element| element > 0 ? array.select { |value| value > 0 }.min : element } '
 
-new_array = array.map { |element| element > 0 ? array.select{ |value| value > 0 }.min : element} 
+new_array = array.map { |element| element > 0 ? array.select { |value| value > 0 }.min : element } 
 
 p new_array
