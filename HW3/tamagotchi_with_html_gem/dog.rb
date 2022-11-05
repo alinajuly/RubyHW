006f9c21d56b49b1a02375db3c448ffcfb33d149
alinajuly <bluealinajuly90@gmail.com>
2022-11-05 14:44:04 +0200

    add pet_info

--- HW3/tamagotchi_with_html_gem/dog.rb
+++ HW3/tamagotchi_with_html_gem/dog.rb
@@ -1,31 +1,31 @@
 class Dog < Pet
   def play
     super
-    to_show_html("<center><b><font size=7>🐕 #{name} playing with bone 🦴</font></b></center>")
+    to_show_html("<center><b><font size=7>🐕 #{name} playing with bone 🦴</font></b></center>" + pet_info)
   end
 
   def feed
     super
-    to_show_html("<center><b><font size=7>🐶 #{name} loves meat 🥩</font></b></center>")
+    to_show_html("<center><b><font size=7>🐶 #{name} loves meat 🥩</font></b></center>" + pet_info)
   end
 
   def drink
     super
-    to_show_html("<center><b><font size=7>🐶 💦 #{name} slurp aloud</font></b></center>")
+    to_show_html("<center><b><font size=7>🐶 💦 #{name} slurp aloud</font></b></center>" + pet_info)
   end
 
   def hug
     super
-    to_show_html("<center><b><font size=7>🐶 #{name} loves you too WOOF! 💓 </font></b></center>")
+    to_show_html("<center><b><font size=7>🐶 #{name} loves you too WOOF! 💓 </font></b></center>" + pet_info)
   end
 
   def walk
     super
-    to_show_html("<center><b><font size=7>🐕 #{name} runs up to every tree 🌳 🌲</font></b></center>")
+    to_show_html("<center><b><font size=7>🐕 #{name} runs up to every tree 🌳 🌲</font></b></center>" + pet_info)
   end
 
   def put_to_bed
     super
-    to_show_html("<center><b><font size=7>💤 sleep tight #{name}</font></b></center>")
+    to_show_html("<center><b><font size=7>💤 sleep tight #{name}</font></b></center>" + pet_info)
   end
 end
