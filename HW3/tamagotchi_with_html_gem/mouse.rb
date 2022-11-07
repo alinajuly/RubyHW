85cf16a7d03323cd237a653d60acc9fb334daa3d
alinajuly <bluealinajuly90@gmail.com>
2022-11-07 13:41:03 +0200

    Replaced the emoji with dec-code

--- HW3/tamagotchi_with_html_gem/mouse.rb
+++ HW3/tamagotchi_with_html_gem/mouse.rb
@@ -1,31 +1,31 @@
 class Mouse < Pet
   def play 
     super
-    to_show_html("<center><b><font size=7>🐁 #{name} playing with tail</font></b></center>"  + pet_info)
+    to_show_html("<center><b><font size=7>&#128001 #{name} playing with tail</font></b></center>"  + pet_info)
   end
 
   def feed
     super
-    to_show_html("<center><b><font size=7>🐭 🧀 so tasty pipipipi</font></b></center>" + pet_info)
+    to_show_html("<center><b><font size=7>&#128045 &#129472 so tasty pipipipi</font></b></center>" + pet_info)
   end
 
   def drink
     super
-    to_show_html("<center><b><font size=7>🐭 💦 #{name} slurp aloud</font></b></center>" + pet_info)
+    to_show_html("<center><b><font size=7>&#128045 #{name} slurp aloud &#128166</font></b></center>" + pet_info)
   end
 
   def hug
     super
-    to_show_html("<center><b><font size=7>🐭 #{name} loves you too PIPIPI! 💓</font></b></center>" + pet_info)
+    to_show_html("<center><b><font size=7>&#128045 #{name} loves you too PIPIPI! &#128147</font></b></center>" + pet_info)
   end
 
   def walk
     super
-    to_show_html("<center><b><font size=7>🐁 #{name} enjoy the green grass 🍃</font></b></center>" + pet_info)
+    to_show_html("<center><b><font size=7>&#128001 #{name} enjoy the green grass &#128062; &#127811</font></b></center>" + pet_info)
   end
   
   def put_to_bed
     super
-    to_show_html("<center><b><font size=7>💤 sleep tight #{name}</font></b></center>" + pet_info)
+    to_show_html("<center><b><font size=7>&#128164 sleep tight #{name}</font></b></center>" + pet_info)
   end
 end
