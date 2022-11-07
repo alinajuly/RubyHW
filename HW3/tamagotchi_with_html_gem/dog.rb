c78e290c9f86adc2376545ee14bc0b4ee040ef7d
alinajuly <bluealinajuly90@gmail.com>
2022-11-07 13:40:20 +0200

    Replaced the emoji with dec-code

--- HW3/tamagotchi_with_html_gem/dog.rb
+++ HW3/tamagotchi_with_html_gem/dog.rb
@@ -1,31 +1,31 @@
 class Dog < Pet
   def play
     super
-    to_show_html("<center><b><font size=7>🐕 #{name} playing with bone 🦴</font></b></center>" + pet_info)
+    to_show_html("<center><b><font size=7>&#128021 #{name} playing with bone &#129460</font></b></center>" + pet_info)
   end
 
   def feed
     super
-    to_show_html("<center><b><font size=7>🐶 #{name} loves meat 🥩</font></b></center>" + pet_info)
+    to_show_html("<center><b><font size=7>&#128054 #{name} loves meat &#129385</font></b></center>" + pet_info)
   end
 
   def drink
     super
-    to_show_html("<center><b><font size=7>🐶 💦 #{name} slurp aloud</font></b></center>" + pet_info)
+    to_show_html("<center><b><font size=7>&#128054 #{name} slurp aloud &#128166</font></b></center>" + pet_info)
   end
 
   def hug
     super
-    to_show_html("<center><b><font size=7>🐶 #{name} loves you too WOOF! 💓 </font></b></center>" + pet_info)
+    to_show_html("<center><b><font size=7>&#128054 #{name} loves you too WOOF! &#128147</font></b></center>" + pet_info)
   end
 
   def walk
     super
-    to_show_html("<center><b><font size=7>🐕 #{name} runs up to every tree 🌳 🌲</font></b></center>" + pet_info)
+    to_show_html("<center><b><font size=7>&#128021 #{name} runs up to every tree &#128062; &#127794; &#127795</font></b></center>" + pet_info)
   end
 
   def put_to_bed
     super
-    to_show_html("<center><b><font size=7>💤 sleep tight #{name}</font></b></center>" + pet_info)
+    to_show_html("<center><b><font size=7>&#128164 sleep tight #{name}</font></b></center>" + pet_info)
   end
 end
