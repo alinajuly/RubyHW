cc65eb2783cc10a79578f9b766f47ce446fad2b2
alinajuly <bluealinajuly90@gmail.com>
2022-11-07 13:39:33 +0200

    Replaced the emoji with dec-code

--- HW3/tamagotchi_with_html_gem/cat.rb
+++ HW3/tamagotchi_with_html_gem/cat.rb
@@ -1,31 +1,31 @@
 class Cat < Pet
   def play 
     super
-    to_show_html("<center><b><font size=7>😺 #{name} playing with your hand 👋</font></b></center>" + pet_info)
+    to_show_html("<center><b><font size=7>&#128568 #{name} playing with your hand &#128075</font></b></center>" + pet_info)
   end
 
   def feed
     super
-    to_show_html("<center><b><font size=7>😼 🧆 so tasty mrrmrrrmrrmmmrr</font></b></center>" + pet_info)
+    to_show_html("<center><b><font size=7>&#127835 so tasty mrrmrrrmrrmmmrr &#128568</font></b></center>" + pet_info)
   end
 
   def drink
     super
-    to_show_html("<center><b><font size=7>💦 #{name} slurp aloud</font></b></center>" + pet_info)
+    to_show_html("<center><b><font size=7>&#128568 #{name} slurp aloud &#128166</font></b></center>" + pet_info)
   end
 
   def hug
     super
-    to_show_html("<center><b><font size=7>😻 😽 #{name} loves you too MEOW! 💓</font></b></center>" + pet_info)
+    to_show_html("<center><b><font size=7>&#128571; &#128573 #{name} loves you too MEOW! &#128147</font></b></center>" + pet_info)
   end
 
   def walk
     super
-    to_show_html("<center><b><font size=7>😸 #{name} chasing butterflies 🦋</font></b></center>" + pet_info)
+    to_show_html("<center><b><font size=7>&#128008 #{name} chasing butterflies &#129419</font></b></center>" + pet_info)
   end
 
   def put_to_bed
     super
-    to_show_html("<center><b><font size=7>💤 sleep tight #{name}</font></b></center>" + pet_info)
+    to_show_html("<center><b><font size=7>&#128164 sleep tight #{name}</font></b></center>" + pet_info)
   end
 end
