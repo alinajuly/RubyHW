334eec3a3962699973d4759b79ed413564415b8c
alinajuly <bluealinajuly90@gmail.com>
2022-12-06 21:05:06 +0200

    added a tag Article, adeed scope to show last 10 comments

---
+++ HW5-6-7/blog/app/controllers/api/v1/tags_controller.rb
@@ -0,0 +1,51 @@
+class Api::V1::TagsController < ApplicationController
+  before_action :set_tag, only: %i[ show update destroy ]
+
+  # GET /api/v1/tags
+  def index
+    @tags = Tag.all
+
+    render json: @tags
+  end
+
+  # GET /api/v1/tags/1
+  def show
+    render json: @tag
+  end
+
+  # POST /api/v1/tags
+  def create
+    @tag = Tag.new(tag_params)
+
+    if @tag.save
+      render json: @tag, status: :created
+    else
+      render json: @tag.errors, status: :unprocessable_entity
+    end
+  end
+
+  # PATCH/PUT /api/v1/tags/1
+  def update
+    if @tag.update(tag_params)
+      render json: @tag
+    else
+      render json: @tag.errors, status: :unprocessable_entity
+    end
+  end
+
+  # DELETE /api/v1/tags/1
+  def destroy
+    @tag.destroy
+  end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_tag
+      @tag = Tag.find(params[:id])
+    end
+
+    # Only allow a list of trusted parameters through.
+    def tag_params
+      params.require(:tag).permit(:title)
+    end
+end
---
+++ HW5-6-7/blog/app/models/article.rb
@@ -0,0 +1,10 @@
+class Article < ApplicationRecord
+  has_many :comments, dependent: :destroy
+  has_many :articletags
+  has_many :tags, through: :articletags
+
+  scope :last_ten_comments, ->(article) { article.comments.last(10) }
+
+  validates :body, presence: true, length: { minimum: 1 }
+  validates :title, presence: true, length: { minimum: 1 }
+end
