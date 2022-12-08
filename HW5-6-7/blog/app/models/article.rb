1a861fccb14a5839edf2fbd003f4154b835748e0
alinajuly <bluealinajuly90@gmail.com>
2022-12-08 20:07:01 +0200

    refactoring show last 10 comments

--- HW5-6-7/blog/app/models/article.rb
+++ HW5-6-7/blog/app/models/article.rb
@@ -4,8 +4,6 @@ class Article < ApplicationRecord
   has_many :tags, through: :articletags
   has_many :likes, as: :likeable
 
-  scope :last_ten_comments, -> { order(created_at: :desc).last(10) }
-
   validates :body, presence: true, length: { minimum: 1 }
   validates :title, presence: true, length: { minimum: 1 }
 end
