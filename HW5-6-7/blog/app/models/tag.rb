e27ab06048bc4192c598e830617bf8f00ef03185
alinajuly <bluealinajuly90@gmail.com>
2022-12-08 13:24:30 +0200

    added validates uniqueness

--- HW5-6-7/blog/app/models/tag.rb
+++ HW5-6-7/blog/app/models/tag.rb
@@ -2,5 +2,5 @@ class Tag < ApplicationRecord
   has_many :article_tags
   has_many :articles, through: :article_tags
 
-  validates :title, presence: true, length: { minimum: 1 }
+  validates :title, presence: true, uniqueness: true, length: { minimum: 1 }
 end
