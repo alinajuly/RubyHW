ccbaa066d25f43c65e6e4517878a2e49d3b93b39
alinajuly <bluealinajuly90@gmail.com>
2022-12-08 13:22:18 +0200

    refactoring duplicate

--- HW5-6-7/blog/db/seeds.rb
+++ HW5-6-7/blog/db/seeds.rb
@@ -6,13 +6,11 @@
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
 
-Author.create({ name: 'Irina Sarapina' })
-Author.create({ name: 'Nataliia Kolesnikova' })
-Article.create({ title: 'Article1', body: 'text of body' })
-Article.create({ title: 'Article2', body: 'another text of body' })
-Comment.create({ body: 'First comment', status: 0, author_id: 1, article: Article.first })
-Comment.create({ body: 'Second comment', status: 1, author_id: 2, article: Article.second })
-Tag.create({ title: 'First tag title' })
-Tag.create({ title: 'Second tag title' })
-Like.create({ likeable_type: 'Article', likeable_id: Article.first })
-Like.create({ likeable_type: 'Comment', likeable_id: Comment.first })
+authors = Author.create([{ name: 'Irina Sarapina' }, { name: 'Nataliia Kolesnikova' }])
+articles = Article.create([{ title: 'Article1', body: 'text of body' }, 
+                          { title: 'Article2', body: 'another text of body' }])
+comments = Comment.create([{ body: 'First comment', status: 0, author_id: 1, article: Article.first }, 
+                          { body: 'Second comment', status: 1, author_id: 2, article: Article.second }])
+tags = Tag.create([{ title: 'First tag title' }, { title: 'Second tag title' }])
+likes = Like.create([{ likeable_type: 'Article', likeable_id: Article.first }, 
+                    { likeable_type: 'Comment', likeable_id: Comment.first }])
