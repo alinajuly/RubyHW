17e8f2b68a8bb00d12b8ad3ded1bb80deac89ed6
alinajuly <bluealinajuly90@gmail.com>
2022-12-12 21:21:32 +0200

    update the name for  join table, update validates

--- HW5-6-7/blog/app/models/article.rb
+++ HW5-6-7/blog/app/models/article.rb
@@ -1,9 +1,8 @@
 class Article < ApplicationRecord
   has_many :comments, dependent: :destroy
   has_many :articletags
-  has_many :tags, through: :articletags
+  has_many :tags, through: :article_tags
   has_many :likes, as: :likeable
 
-  validates :body, presence: true, length: { minimum: 1 }
-  validates :title, presence: true, length: { minimum: 1 }
+  validates :body, :title, presence: true, length: { minimum: 1 }
 end
