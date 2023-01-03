b56d4c6744ca9a863bbd31f01cf49123cd08861a
alinajuly <bluealinajuly90@gmail.com>
2023-01-03 20:11:17 +0200

    added validates for product price, removed button for product at products_show

--- HW10/shop/app/models/product.rb
+++ HW10/shop/app/models/product.rb
@@ -1,6 +1,7 @@
 class Product < ApplicationRecord
   belongs_to :category
 
-  validates :name, :description, :price, :category_id, presence: true, length: { minimum: 1 }
+  validates :name, :description, :category_id, presence: true
   validates :image, presence: false
+  validates :price, presence: true, numericality: { greater_than_or_equal_to: 0 }
 end
