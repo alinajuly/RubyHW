be8734fac1f4b3d191c291050c4d4a12d1ad07a4
alinajuly <bluealinajuly90@gmail.com>
2023-01-16 18:02:56 +0200

    fixed according issues

--- HW11/shop/app/models/cart.rb
+++ HW11/shop/app/models/cart.rb
@@ -1,5 +1,5 @@
 class Cart < ApplicationRecord
-  has_many :line_items, dependent: :nullify
+  has_many :line_items, dependent: :destroy
   has_many :products, through: :line_items
   has_one :order
 
--- HW11/shop/app/models/product.rb
+++ HW11/shop/app/models/product.rb
@@ -1,8 +1,8 @@
 class Product < ApplicationRecord
   belongs_to :category
-  has_many :line_items, dependent: :nullify
+  has_many :line_items, dependent: :destroy
 
   validates :name, :description, :category_id, presence: true
   validates :image, presence: false
-  validates :price, presence: true, numericality: { greater_than_or_equal_to: 0 }
+  validates :price, presence: true, numericality: { greater_than: 0 }
 end
