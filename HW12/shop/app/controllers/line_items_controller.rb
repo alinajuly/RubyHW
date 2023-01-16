3ad361daa4da5c552bb2c2db03ddcb2274487d41
alinajuly <bluealinajuly90@gmail.com>
2023-01-16 23:51:44 +0200

    refactoring syntaxis, controllers

--- HW12/shop/app/controllers/line_items_controller.rb
+++ HW12/shop/app/controllers/line_items_controller.rb
@@ -1,6 +1,6 @@
 class LineItemsController < ApplicationController
   before_action :authenticate_user!
-  before_action :set_line_item, only: %i[destroy increase_quantity decrease_quantity]
+  before_action :set_line_item, only: %i[destroy update]
 
   def create
     product = Product.find(params[:product_id])
@@ -15,21 +15,17 @@ class LineItemsController < ApplicationController
     redirect_to cart_path, notice: "#{product.name} was successfully added to the cart"
   end
 
-  def update; end
+  def update
+    @line_item.quantity -= 1 if params[:change_quantity] == 'decrease'
+    @line_item.quantity += 1 if params[:change_quantity] == 'increase'
+    @line_item.update(quantity: @line_item.quantity)
+
+    redirect_to cart_path(current_cart)
+  end
 
   def destroy
     @line_item.destroy
 
-    redirect_to cart_path(current_cart)
-  end
-
-  def decrease_quantity
-    @line_item.update(quantity: @line_item.quantity - 1)
-    redirect_to cart_path(current_cart)
-  end
-
-  def increase_quantity
-    @line_item.update(quantity: @line_item.quantity + 1)
     redirect_to cart_path(current_cart)
   end
 
--- HW12/shop/config/routes.rb
+++ HW12/shop/config/routes.rb
@@ -4,15 +4,12 @@ Rails.application.routes.draw do
   devise_for :users
   resources :products
   resources :categories
-  resources :orders
+  resources :orders, only: %i[index create show]
+  resources :line_items, only: %i[create update destroy]
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   root 'products#index'
   get '/cart', to: 'carts#show', as: :cart
-  resources :line_items, only: %i[create update destroy]
-  post 'line_items/:id/increase_quantity', to: 'line_items#increase_quantity', as: 'line_item_increase'
-  post 'line_items/:id/decrease_quantity', to: 'line_items#decrease_quantity', as: 'line_item_decrease'
-  resources :orders, only: %i[index create show]
   put 'orders/:id/order_pay', to: 'orders#order_pay', as: 'order_pay'
 end
