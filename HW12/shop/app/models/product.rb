74c7702cef35bdd96340c9c171bc19ff7610065d
alinajuly <bluealinajuly90@gmail.com>
2023-01-19 21:17:55 +0200

    added annotate gem

--- HW12/shop/app/models/product.rb
+++ HW12/shop/app/models/product.rb
@@ -1,3 +1,16 @@
+# == Schema Information
+#
+# Table name: products
+#
+#  id          :bigint           not null, primary key
+#  name        :string
+#  description :text
+#  price       :decimal(, )
+#  image       :string
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#  category_id :bigint           not null
+#
 class Product < ApplicationRecord
   belongs_to :category
   has_many :line_items, dependent: :destroy
