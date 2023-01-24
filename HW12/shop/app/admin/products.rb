e7bd8ae66b0232c2e6cc219e87e803b1f1853c42
alinajuly <bluealinajuly90@gmail.com>
2023-01-24 21:54:56 +0200

    fixed n+1

--- HW12/shop/app/admin/products.rb
+++ HW12/shop/app/admin/products.rb
@@ -1,18 +1,15 @@
 ActiveAdmin.register Product do
-
-  # See permitted parameters documentation:
-  # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
-  #
-  # Uncomment all parameters which should be permitted for assignment
-  #
   permit_params :name, :description, :price, :image, :category_id
-  #
-  # or
-  #
-  # permit_params do
-  #   permitted = [:name, :description, :price, :image, :category_id]
-  #   permitted << :other if params[:action] == 'create' && current_user.admin?
-  #   permitted
-  # end
-  
+
+  includes :category
+
+  form do |f|
+    f.inputs do
+      f.input :name
+      f.input :description
+      f.input :price
+      f.input :image, as: :file
+    end
+    f.actions
+  end
 end
--- HW12/shop/app/models/product.rb
+++ HW12/shop/app/models/product.rb
@@ -17,5 +17,5 @@ class Product < ApplicationRecord
 
   validates :name, :description, :category_id, presence: true
   validates :image, presence: false
-  validates :price, presence: true, numericality: { greater_than: 0 }
+  validates :price, presence: true, numericality: { greater_than_or_equal_to: 0 }
 end
